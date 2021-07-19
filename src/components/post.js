2492b83e9c5f8a0f9bce0f725534fd543bc2bb11
NIHAL TIWARI <nihaltiwari28@gmail.com>
2021-07-19 18:12:48 +0530

    Add Blog and Blog Internal Page

--- src/components/post.js
+++ src/components/post.js
@@ -1,131 +1,92 @@
-import React from 'react';
-function Post()
-{
-    return ( <div>
-          <section class="recentpost">
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+function Post() {
+  const [article, setArticle] = useState([]);
+
+  useEffect(() => {
+    async function getAticles() {
+      const article = await axios.get(
+        "https://trw-backend-api.herokuapp.com/blog/Blog1s"
+      );
+      setArticle(article.data);
+    }
+
+    getAticles();
+  }, []);
+  return (
+    <div>
+      <section className="recentpost">
         <h1>Recent Posts</h1>
         <p>Rhoncus tempor nunc, praesent amet eu hac tortor malesuada tellus</p>
-        <div class="recentpost-main">
-          <div style={{alignSelf: 'center'}}>
-            <button class="arrows">
-              <i class="fas fa-chevron-left"></i
-              ><i class="fas fa-chevron-left"></i>
+        <div className="recentpost-main">
+          <div style={{ alignSelf: "center" }}>
+            <button className="arrows">
+              <i className="fas fa-chevron-left"></i>
+              <i className="fas fa-chevron-left"></i>
             </button>
           </div>
-          <div class="card">
-            <div>
-              <img src="/assests/images/pen.jpg" alt="" />
-            </div>
-            <div class="content-card">
-              <div>
-                <span class="ali">Aliquam auctor</span>
-                <span class="head">HEALING</span>
+          {article.map((data) => {
+            return (
+              <div className="card">
+                <div>
+                  <img src={data.image} alt="" />
+                </div>
+                <div className="content-card">
+                  <div>
+                    <span className="ali">{data.title}</span>
+                    <span className="head">{data.category}</span>
+                  </div>
+                  <div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      In doloribus, accusantium, exercitationem velit hic et
+                      consequatur aperiam qui cupiditate, similique alias rem
+                      quas aliquam eligendi quam quibusdam unde minima vitae.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    className="button"
+                    style={{
+                      borderRadius: "0",
+                      width: "100%",
+                      margin: "0",
+                      borderBottomLeftRadius: "12px",
+                      borderBottomRightRadius: "12px",
+                    }}
+                  >
+                    Read More
+                    <i
+                      className="fas fa-chevron-right"
+                      style={{ float: "right" }}
+                    ></i>
+                    <i
+                      className="fas fa-chevron-right"
+                      style={{ float: "right" }}
+                    ></i>
+                  </button>
+                </div>
               </div>
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-                  doloribus, accusantium, exercitationem velit hic et
-                  consequatur aperiam qui cupiditate, similique alias rem quas
-                  aliquam eligendi quam quibusdam unde minima vitae.
-                </p>
-              </div>
-            </div>
-            <div>
-              <button
-                class="button"
-                style={{
-                    borderRadius: '0',
-                    width: '100%',
-                    margin: '0',
-                    borderBottomLeftRadius: '12px',
-                    borderBottomRightRadius: '12px'}}
-              >
-                Read More
-                <i class="fas fa-chevron-right" style={{float: 'right'}}></i
-                ><i class="fas fa-chevron-right" style={{float: 'right'}}></i>
-              </button>
-            </div>
-          </div>
-          <div class="card">
-            <div>
-              <img src="/assests/images/stones-card.jfif" alt="" />
-            </div>
-            <div class="content-card">
-              <div>
-                <span class="ali">Aliquam auctor</span>
-                <span class="lead">LEADING</span>
-              </div>
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-                  doloribus, accusantium, exercitationem velit hic et
-                  consequatur aperiam qui cupiditate, similique alias rem quas
-                  aliquam eligendi quam quibusdam unde minima vitae.
-                </p>
-              </div>
-            </div>
-            <div>
-              <button
-                class="button"
-                style={{
-                    borderRadius: '0',
-                    width: '100%',
-                    margin: '0',
-                    borderBottomLeftRadius: '12px',
-                    borderBottomRightRadius: '12px'}}
-              >
-                Read More
-                <i class="fas fa-chevron-right" style={{float: 'right'}}></i
-                ><i class="fas fa-chevron-right" style={{float: 'right'}}></i>
-              </button>
-            </div>
-          </div>
-          <div class="card">
-            <div>
-              <img src="/assests/images/stones-card.jfif" alt="" />
-            </div>
-            <div class="content-card">
-              <div>
-                <span class="ali">Aliquam auctor</span>
-                <span class="head">HEALING</span>
-              </div>
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. In
-                  doloribus, accusantium, exercitationem velit hic et
-                  consequatur aperiam qui cupiditate, similique alias rem quas
-                  aliquam eligendi quam quibusdam unde minima vitae.
-                </p>
-              </div>
-            </div>
-            <div>
-              <button
-                class="button"
-                style={{
-                  borderRadius: '0',
-                  width: '100%',
-                  margin: '0',
-                  borderBottomLeftRadius: '12px',
-                  borderBottomRightRadius: '12px'}}
-              >
-                Read More
-                <i class="fas fa-chevron-right" style={{float: 'right'}}></i
-                ><i class="fas fa-chevron-right" style={{float: 'right'}}></i>
-              </button>
-            </div>
-          </div>
-          <div style={{alignSelf: 'center'}}>
-            <button class="arrows">
-              <i class="fas fa-chevron-right"></i
-              ><i class="fas fa-chevron-right"></i>
+            );
+          })}
+          <div style={{ alignSelf: "center" }}>
+            <button className="arrows">
+              <i className="fas fa-chevron-right"></i>
+              <i className="fas fa-chevron-right"></i>
             </button>
           </div>
         </div>
         <div>
-          <img style={{marginTop: '80px'}} src="/assests/images/Group1.svg" alt="" />
+          <img
+            style={{ marginTop: "80px" }}
+            src="/assests/images/Group1.svg"
+            alt=""
+          />
         </div>
       </section>
-</div>)
+    </div>
+  );
 }
 
 export default Post;
--- src/routing.js
+++ src/routing.js
@@ -3,17 +3,21 @@ import Home from "./components/home";
 import About from "./components/about";
 import ContactUs from "./components/contactus";
 import EventsInternal from "./components/eventsinternal";
+import Blog from "./components/blog";
+import BlogInternal from "./components/bloginternal";
 function Routing() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/about" component={About}></Route>
-                <Route exact path="/contactus" component={ContactUs}></Route>
-                <Route exact path="/events" component={EventsInternal}></Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/contactus" component={ContactUs}></Route>
+        <Route exact path="/events" component={EventsInternal}></Route>
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/blogs" component={BlogInternal} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default Routing;
