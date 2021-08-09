03c6ce4f05854766fc8efe4d5d00d58186437023
lakshy <luckyron1279@gmail.com>
2021-08-09 18:14:23 +0530

    add validator in contactus

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from  'axios';
 import PropTypes from "prop-types";
+import SimpleReactValidator from 'simple-react-validator';
 class Contact extends React.Component
 {
   constructor(props) {
@@ -16,9 +17,92 @@ class Contact extends React.Component
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSubmit1 = this.handleSubmit1.bind(this);
-    
-}
+    this.validator = new SimpleReactValidator({
+      className: "text-danger",
+      validators: {
+          passwordvalid: {
+              message:
+                  "The :attribute must be at least 6 and at most 30 with 1 numeric,1 special charac" +
+                  "ter and 1 alphabet.",
+              rule: function (val, params, validator) {
+                  return (
+                      validator.helpers.testRegex(
+                          val,
+                          /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z]).{6,30}$/i
+                      ) && params.indexOf(val) === -1
+                  );
+              },
+          },
+          passwordMismatch: {
+              message: "confirm password must match with password field.",
+              rule: function (val, params, validator) {
+                  return document.getElementById("password_input").value === val
+                      ? true
+                      : false;
+              },
+          },
+          whitespace: {
+              message: "The :attribute not allowed first whitespace   characters.",
+              rule: function (val, params, validator) {
+                  return (
+                      validator.helpers.testRegex(val, /[^\s\\]/) &&
+                      params.indexOf(val) === -1
+                  );
+              },
+          },
+          specialChar: {
+              message: "The :attribute not allowed special   characters.",
+              rule: function (val, params, validator) {
+                  return (
+                      validator.helpers.testRegex(val, /^[ A-Za-z0-9_@./#&+-]*$/i) &&
+                      params.indexOf(val) === -1
+                  );
+              },
+          },
+          specialCharText: {
+              message: "The :attribute may only contain letters, dot and spaces.",
+              rule: function (val, params, validator) {
+                  return (
+                      validator.helpers.testRegex(val, /^[ A-Za-z_@./#&+-]*$/i) &&
+                      params.indexOf(val) === -1
+                  );
+              },
+          },
 
+          zip: {
+              message: "Invalid Pin Code",
+              rule: function (val, params, validator) {
+                  return (
+                      validator.helpers.testRegex(val, /^(\d{5}(\d{4})?)?$/i) &&
+                      params.indexOf(val) === -1
+                  );
+              },
+          },
+          website: {
+              message: "The Url should be example.com ",
+              rule: function (val, params, validator) {
+                  return (
+                      validator.helpers.testRegex(
+                          val,
+                          /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+                      ) && params.indexOf(val) === -1
+                  );
+              },
+          },
+          Fax: {
+              message: "Invalid fax number ",
+              rule: function (val, params, validator) {
+                  return (
+                      validator.helpers.testRegex(
+                          val,
+                          /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/i
+                      ) && params.indexOf(val) === -1
+                  );
+              },
+          },
+      },
+  });
+}
 onChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
@@ -28,18 +112,26 @@ onChange(event) {
   {
     e.preventDefault();
     console.log(this.state.subscribeemail);
-    axios
-        .post(
-          "https://lakshy12.herokuapp.com/contact/add_subscriber",
-          {
-              email:this.state.subscribeemail,
+    if(this.validator.allValid())
+        {
+        axios
+            .post(
+              "https://lakshy12.herokuapp.com/contact/add_subscriber",
+              {
+                  email:this.state.subscribeemail,
+              }
+            )
+            .then((response)=> {
+              // handle success
+              this.props.history.push("/");
+              console.log(response.data);
+            })
           }
-        )
-        .then((response)=> {
-          // handle success
-          window.location.reload();
-          console.log(response.data);
-        })    
+          else
+          {
+            this.validator.showMessages();
+             this.forceUpdate();
+          }    
   }
   handleSubmit(e) {
     e.preventDefault();
@@ -50,6 +142,8 @@ onChange(event) {
       // formdata.append("email", this.state.email);
       // formdata.append("reason", this.state.reason);
       // formdata.append("message", this.state.message);
+    if(this.validator.allValid())
+    {
       axios
         .post(
           "https://lakshy12.herokuapp.com/contact/add_contact",
@@ -65,7 +159,12 @@ onChange(event) {
           // handle success
           window.location.reload();
           console.log(response.data);
-        })    
+        })
+      }
+      else {
+        this.validator.showMessages();
+        this.forceUpdate();
+      } 
     }
   render()
   {
@@ -91,6 +190,11 @@ onChange(event) {
                     placeholder="yourname@address.com"
                     class="input"
                   />
+                      {this.validator.message(
+                                                            "email",
+                                                            this.state.email,
+                                                            "required|whitespace|min:1|max:80"
+                                                        )}
                 &nbsp;&nbsp;&nbsp;
                 <button class="button">Subscribe</button>
               </form>
@@ -101,6 +205,11 @@ onChange(event) {
               <div class="input-box" style={{width:'244px'}}>
                 <p>Full Name</p>
                 <input type="text" class="input" name="name" value={this.state.name} onChange={this.onChange} required />
+                {this.validator.message(
+                                                            "name",
+                                                            this.state.name,
+                                                            "required|whitespace|min:1|max:40"
+                                                        )}
               </div>
               <div class="input-box row1" style={{width:'244px'}}>
                 <p>Email</p>
@@ -113,6 +222,11 @@ onChange(event) {
                      class="input"
                     required
                   />
+                   {this.validator.message(
+                                                            "Email",
+                                                            this.state.email,
+                                                            "required|whitespace|min:1|max:100"
+                                                        )}
               </div>
             </div>
             <div class="flex-row">
@@ -148,6 +262,11 @@ onChange(event) {
                     style={{width: '100%', paddingBottom: '30px',overflowY:"scroll"}}
                     required
                   ></textarea>
+                   {this.validator.message(
+                                                            "Message",
+                                                            this.state.message,
+                                                            "required|whitespace|min:1|max:1000"
+                                                        )}
               </div>
             </div>
             <div class="flex-row" style={{alignSelf: 'flex-end'}}>
--- src/components/contactus.js
+++ src/components/contactus.js
@@ -3,6 +3,7 @@ import Navbar from './navbar';
 import Footer from './footer';
 import PropTypes from "prop-types";
 import axios from 'axios';
+import SimpleReactValidator from 'simple-react-validator';
 class  ContactUs extends React.Component
 {
     constructor(props) {
@@ -18,7 +19,91 @@ class  ContactUs extends React.Component
         this.onChange = this.onChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleSubmit1 = this.handleSubmit1.bind(this);
-        
+        this.validator = new SimpleReactValidator({
+          className: "text-danger",
+          validators: {
+              passwordvalid: {
+                  message:
+                      "The :attribute must be at least 6 and at most 30 with 1 numeric,1 special charac" +
+                      "ter and 1 alphabet.",
+                  rule: function (val, params, validator) {
+                      return (
+                          validator.helpers.testRegex(
+                              val,
+                              /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z]).{6,30}$/i
+                          ) && params.indexOf(val) === -1
+                      );
+                  },
+              },
+              passwordMismatch: {
+                  message: "confirm password must match with password field.",
+                  rule: function (val, params, validator) {
+                      return document.getElementById("password_input").value === val
+                          ? true
+                          : false;
+                  },
+              },
+              whitespace: {
+                  message: "The :attribute not allowed first whitespace   characters.",
+                  rule: function (val, params, validator) {
+                      return (
+                          validator.helpers.testRegex(val, /[^\s\\]/) &&
+                          params.indexOf(val) === -1
+                      );
+                  },
+              },
+              specialChar: {
+                  message: "The :attribute not allowed special   characters.",
+                  rule: function (val, params, validator) {
+                      return (
+                          validator.helpers.testRegex(val, /^[ A-Za-z0-9_@./#&+-]*$/i) &&
+                          params.indexOf(val) === -1
+                      );
+                  },
+              },
+              specialCharText: {
+                  message: "The :attribute may only contain letters, dot and spaces.",
+                  rule: function (val, params, validator) {
+                      return (
+                          validator.helpers.testRegex(val, /^[ A-Za-z_@./#&+-]*$/i) &&
+                          params.indexOf(val) === -1
+                      );
+                  },
+              },
+    
+              zip: {
+                  message: "Invalid Pin Code",
+                  rule: function (val, params, validator) {
+                      return (
+                          validator.helpers.testRegex(val, /^(\d{5}(\d{4})?)?$/i) &&
+                          params.indexOf(val) === -1
+                      );
+                  },
+              },
+              website: {
+                  message: "The Url should be example.com ",
+                  rule: function (val, params, validator) {
+                      return (
+                          validator.helpers.testRegex(
+                              val,
+                              /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+                          ) && params.indexOf(val) === -1
+                      );
+                  },
+              },
+              Fax: {
+                  message: "Invalid fax number ",
+                  rule: function (val, params, validator) {
+                      return (
+                          validator.helpers.testRegex(
+                              val,
+                              /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/i
+                          ) && params.indexOf(val) === -1
+                      );
+                  },
+              },
+          },
+      });
     }
   
     onChange(event) {
@@ -30,6 +115,8 @@ class  ContactUs extends React.Component
       {
         e.preventDefault();
         console.log(this.state.subscribeemail);
+        if(this.validator.allValid())
+        {
         axios
             .post(
               "https://lakshy12.herokuapp.com/contact/add_subscriber",
@@ -41,33 +128,40 @@ class  ContactUs extends React.Component
               // handle success
               this.props.history.push("/");
               console.log(response.data);
-            })    
+            })
+          }
+          else
+          {
+            this.validator.showMessages();
+             this.forceUpdate();
+          }    
       }
       handleSubmit(e) {
         e.preventDefault();
           console.log(this.state);
-          // const formdata = new FormData();
-          // formdata.append("name", this.state.name);
-          // formdata.append("phone", this.state.phone);
-          // formdata.append("email", this.state.email);
-          // formdata.append("reason", this.state.reason);
-          // formdata.append("message", this.state.message);
-          axios
-            .post(
-              "https://lakshy12.herokuapp.com/contact/add_contact",
-              {
-                  name:this.state.name,
-                  phone:this.state.phone,
-                  email:this.state.email,
-                  reason:this.state.reason,
-                  message:this.state.message
-              }
-            )
-            .then((response)=> {
-              // handle success
-              this.props.history.push("/");
-              console.log(response.data);
-            })    
+          if(this.validator.allValid())
+    {
+      axios
+        .post(
+          "https://lakshy12.herokuapp.com/contact/add_contact",
+          {
+              name:this.state.name,
+              phone:this.state.phone,
+              email:this.state.email,
+              reason:this.state.reason,
+              message:this.state.message
+          }
+        )
+        .then((response)=> {
+          // handle success
+          window.location.reload();
+          console.log(response.data);
+        })
+      }
+      else {
+        this.validator.showMessages();
+        this.forceUpdate();
+      }   
         } 
    render()
    {
@@ -99,6 +193,11 @@ class  ContactUs extends React.Component
                     placeholder="yourmail@address.com"
                     class="input"
                   />
+                   {this.validator.message(
+                                                            "email",
+                                                            this.state.email,
+                                                            "required|whitespace|min:1|max:80"
+                                                        )}
                   &nbsp;&nbsp;&nbsp;
                   <button class="contact-button">Subscribe</button>
                 </form>
@@ -109,6 +208,11 @@ class  ContactUs extends React.Component
                 <div class="input-box" style={{width: '244px'}}>
                   <p>Full Name</p>
                   <input type="text" class="input" name="name" value={this.state.name} onChange={this.onChange} required />
+                  {this.validator.message(
+                                                            "name",
+                                                            this.state.name,
+                                                            "required|whitespace|min:1|max:40"
+                                                        )}
                 </div>
                 <div class="input-box row1" style={{width: '244px'}}>
                   <p>Email</p>
@@ -121,6 +225,11 @@ class  ContactUs extends React.Component
                      class="input"
                     required
                   />
+                       {this.validator.message(
+                                                            "Email",
+                                                            this.state.email,
+                                                            "required|whitespace|min:1|max:100"
+                                                        )}
                 </div>
               </div>
               <div class="flex-row">
@@ -160,6 +269,11 @@ class  ContactUs extends React.Component
                     rows="5"
                     required
                   ></textarea>
+                  {this.validator.message(
+                                                            "Message",
+                                                            this.state.message,
+                                                            "required|whitespace|min:1|max:1000"
+                                                        )}
                 </div>
               </div>
               <div class="flex-row" style={{alignSelf: 'flex-end'}}>
